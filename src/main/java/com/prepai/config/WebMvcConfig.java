1b85818575c27a2ef435872694e8f8c3be3e18d1
Aditi Singh <siya07singh05@gmail.com>
2026-08-10 03:22:49 +0530

    Add WebMvcConfig to disable browser caching of static resources on localhost

---
+++ src/main/java/com/prepai/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.prepai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Direct browser to revalidate static files (index.html, app.js, style.css) on every load
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noCache().cachePrivate().mustRevalidate());
+    }
+}

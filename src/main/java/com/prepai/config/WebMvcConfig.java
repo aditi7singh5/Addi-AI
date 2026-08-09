6e3c1a7df36c0dc3cc929a8f3bbe6243208276d2
Aditi Singh <siya07singh05@gmail.com>
2026-08-10 03:26:32 +0530

    Add runtime error diagnostics banner script inside index.html head

--- src/main/java/com/prepai/config/WebMvcConfig.java
+++ src/main/java/com/prepai/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
